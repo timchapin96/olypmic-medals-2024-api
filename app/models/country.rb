b7440bdb0fb14dacba5da6341979fbca23d7b03c
Tim Chapin <chapin.timothys@gmail.com>
2024-08-23 13:43:57 +0900

    Added scopes for params and fixed db config spelling error

--- app/models/country.rb
+++ app/models/country.rb
@@ -4,4 +4,12 @@ class Country < ApplicationRecord
   validates :country_code, presence: true, length: {maximum: 3}, uniqueness: true
   validates :gold, :silver, :bronze, :total, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :total, presence: true, numericality: true
+
+
+  scope :by_country_code, ->(country_code) {where(country_code: country_code)}
+  scope :by_rank, ->(rank) { where(rank: rank) }
+  scope :by_sort_by, -> (field) { order(field) }
+  scope :by_order, -> (direction) { order(created_at: direction) }
+
+  scope :by_medal_type, ->(medal_type, count) { where("#{medal_type} >= ?", count) }
 end
